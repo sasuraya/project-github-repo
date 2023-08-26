45b6273df5adf8fc2ed3a35ae406a8e164535a69
sasuraya <surayasadat@gmail.com>
2023-08-26 16:05:32 -0400

    printing full name

---
+++ src/firstDemo/Test2_Print_FullName.java
@@ -0,0 +1,14 @@
+package firstDemo;
+
+public class Test2_Print_FullName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// print full name sur jan.
+	String fName = "sur";
+	String lName = "jan";
+	String fullName = (fName + " " + lName);
+	System.out.println("Full name is : " + fullName);
+	}
+
+}
