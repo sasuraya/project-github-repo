8ac429755125def5d3a401575b587cfcf79dfa75
sasuraya <surayasadat@gmail.com>
2023-08-26 15:35:13 -0400

    new changes addet

---
+++ src/firstDemo/Test1_Find_Total.java
@@ -0,0 +1,14 @@
+package firstDemo;
+
+public class Test1_Find_Total {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// find the total of num1 and num2
+		int num1 = 10;
+		int num2 = 5;
+		int total = (num1 + num2);
+		System.out.println("Total number it is: " + total);
+	}
+
+}
